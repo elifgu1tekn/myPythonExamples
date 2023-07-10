870e05da746d54a65ffa463252daefc70e952486
elifgu1tekn <138573022+elifgu1tekn@users.noreply.github.com>
2023-07-10 20:03:29 +0300

    Add files via upload

---
+++ veriables.py
@@ -0,0 +1,21 @@
+# 1. örnek
+ad= 'Elif '
+soyAd= 'Gültekin'
+adSoyad= ad + ' ' + soyAd
+cinsiyet= True #kadin
+tcKimlik ='15246618756' #hesaplama falan yapmayacağımız için string olması sıkıntı olmaz
+dogumYil= 2002
+adres= 'İstanbul Başakşehir'
+yas= 2023- dogumYil
+
+print(ad, soyAd, adSoyad, cinsiyet, tcKimlik, dogumYil, adres, yas)
+
+#2. örnek
+order1= 1100
+order2= 1200.5
+order3= 1450.25
+
+print( order1+ order2 + order3)
+
+total= order1+ order2+ order3
+print("Total: ", total)

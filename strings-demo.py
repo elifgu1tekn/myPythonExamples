b9d302a673ec0d3fba6df024c1863053d747bba3
Elif <elifgultekin@marun.edu.tr>
2023-07-11 19:02:39 +0300

    Add files via upload

---
+++ strings-demo.py
@@ -0,0 +1,20 @@
+website = "htpp://www.elifgu1tekn.com"
+repositories = "myPythonExamples"
+
+# 'repositories' dizisinde kaç karakter var?
+print(len(repositories))
+
+# 'website' içinden www karakterlerini al.
+print(website[7:10])
+
+# 'website' içinden com karakterini al.
+print(len(website))
+print(website[23:])
+
+# 'repositories' içinden ilk 3 ve son 3 karakterleri al.
+print(repositories[:3]) 
+print(repositories[-3:])
+
+# 'repositories' ifadesindeki karakterleri tersten yazdır.
+print(repositories[::-1])
+

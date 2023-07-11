a44d3658c07825c32d7ece577a0629c2dabad3f5
Elif <elifgultekin@marun.edu.tr>
2023-07-11 17:12:46 +0300

    Add files via upload

---
+++ type-conversion.py
@@ -0,0 +1,40 @@
+x = 4
+y = 1.5
+name = "Elif"
+isOnline = True
+
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(isOnline))
+
+#int to float
+x = float(x)
+print(x)
+print(type(x))
+
+# float to int
+y= int(y)
+print(y)
+print(type(y))
+
+#int, float to str
+result = str(x) + str(y)
+print(result)
+print(type(result))
+
+#bool to str
+isOnline = str(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+#bool to int
+isOnline = True
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+isOnline_f = False
+isOnline_f = int(isOnline_f)
+print(isOnline_f)
+print(type(isOnline))

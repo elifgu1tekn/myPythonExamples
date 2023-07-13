64264929cf8248598702251816804da4b0d4d7a7
Elif <elifgultekin@marun.edu.tr>
2023-07-13 17:06:36 +0300

    Add files via upload

---
+++ kullanıcıdanAlınan3MarkayıListele.py
@@ -0,0 +1,12 @@
+markalar = []
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+marka = input("Marka: ")
+markalar.append(marka)
+
+print(markalar)
